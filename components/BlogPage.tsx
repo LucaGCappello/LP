5db20df0f14ecad8395bc076126f8a894a543253
LucaGCappello <152432062+LucaGCappello@users.noreply.github.com>
2026-02-24 05:20:49 +0000

    Updated BlogPage.tsx

--- components/BlogPage.tsx
+++ components/BlogPage.tsx
@@ -53,7 +53,7 @@ const BlogPage: React.FC = () => {
       name: 'Scale With Teddy',
       logo: {
         '@type': 'ImageObject',
-        url: '/Logo/1.png'
+        url: 'nexta_no_background.png'
       }
     },
     blogPost: filteredArticles.map(article => ({
